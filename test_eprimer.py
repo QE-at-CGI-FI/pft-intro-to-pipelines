2e0249b623b792ec24649b27dd40039eac3e8383
maaretp <maaret.pyhajarvi@gmail.com>
2025-09-26 01:39:36 +0300

    Add samples

--- test_eprimer.py
+++ test_eprimer.py
@@ -24,12 +24,14 @@ class TestEPrimer:
         ("There is a cat.", 4, 1, 0),
         ("The cat is nowhere to be found.", 7, 2, 0), 
         ("typewriter's", 1, 0, 1),
+        ('"be"', 1, 1, 0),
+        ("<div>be</div>", 1, 1, 0), 
 
     ]
     ids=["past tense", "close spelling", "basic grammar", "hamlet", 
          "possessive maaret", "long text", "file", "html", "empty", "various", 
          "identity", "class membership", "class inclusion", "predication", "ownership", "auxiliary progressive", 
-         "auxiliary passive", "existence", "location", "typewriter apostrophe", ]
+         "auxiliary passive", "existence", "location", "typewriter apostrophe", "doublequoted be", "html be"]
 
     data_revealing_bugs = [
         ("To be or not to be – Hamlet’s dilemma", 8, 2, 1),
@@ -40,10 +42,18 @@ class TestEPrimer:
         ("The electron functions as a particle.", 6, 1, 0),
         ("being", 1, 1, 0),
         ("typesetter’s", 1, 0, 1),
+        ("I'm", 2, 1, 0), 
+        ("'Be'", 1, 1, 0),
+        ("\n", 1, 0, 0),
+        ("first\nsecond", 1, 0, 0),
+        ("ain't, amn't", 2, 2, 0),
+        ("Hanna's Esa's Meera's Süëss-O'Reggio's or Okechukwu's", 6, 0, 4),
+
     ]
     ids_revealing_bugs=["bug_incorrect_word_count", "bug bible incorrect word count", "bug possessive ending", 
                         "bug plural possessive ending", "bug all contractions", "whats a word", "dual meaning", 
-                        "typesetter apostrophe"]
+                        "typesetter apostrophe", "counting words", "singlequoted be", "newline", "newline with text", 
+                        "nonstandard contractions", "two part names" ]
 
     @pytest.mark.parametrize("input_text, expect_1, expect_2, expect_3", data, ids=ids)
     def test_ones_that_work(self, page_to_url, input_text, expect_1, expect_2, expect_3):
